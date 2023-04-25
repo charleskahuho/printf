aaa22a5f68f1c7886029e32f4fb81d4922ac1d7a
charleskahuho <ckcharles004@gmail.com>
2023-04-25 09:04:29 -0700

    main.h file updated

--- main.h
+++ main.h
@@ -1,3 +1,12 @@
 #ifndef MAIN
 #define MAIN "main.h"
+void _print_binary(int n, int *k);
+void _rot13(char *s, int *k);
+void _rev_string(char *s, int *k);
+int _strlen(char *s);
+ int _putchar(char c, int *k);
+void _puts(char *str, int *k);
+int _printf(const char *format, ...);
+void _print_number(int n, int *k);
 
+#endif
